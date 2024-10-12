fe7eb32845e5327c8639626dc98c58ed09c6add0
Nikhil5003 <nikhil9812382543@gmail.com>
2024-10-12 19:14:42 +0530

    HOC Pattern added

---
+++ src/DesignPatterns/HOCPattern.js
@@ -0,0 +1,7 @@
+// this is a pattern where we take one component as a props in another functional components and write some logic amd render that component by passing down some props for better reusability
+//  function abc(ELe , data){
+// return <Ele {...data} />
+
+// }
+
+//Example is connect in redux where we connect the component to redux with some states and actions
